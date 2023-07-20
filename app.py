3bf46fa45e3bdb790deb7b30da79869d79d2b2ce
Your Name <you@example.com>
2023-07-19 20:01:44 -0600

    before big update

--- app.py
+++ app.py
@@ -36,5 +36,32 @@ def getDestrezas():
         print(err)
         return jsonify({'message': 'Error en la consulta a la base de datos'}), 500
 
+
+@app.route('/api/getIndicadores', methods=["POST"])
+def getIndicadores():
+    filters = request.get_json()
+    if not filters:
+        return jsonify({'message': 'Bad Request: JSON data required'}), 400
+
+    try:
+        criterio_de_evaluacioni = filters["criterio_de_evaluacioni"]
+
+        response = requests.get(
+            f'{database_url}/Indicadores?select=indicador&criterio_de_evaluacioni=eq.{criterio_de_evaluacioni}', 
+            headers=headers
+        )
+
+        if response.status_code != 200:
+            print(f"Error response from database: {response.content}")
+            return jsonify({'message': 'Error en la consulta a la base de datos'}), 500
+
+        indicadores = [row['indicador'] for row in response.json()]
+        return jsonify({'indicadores': indicadores}), 200
+    except KeyError:
+        return jsonify({'message': "Error: Falta uno o más detalles requeridos en los filtros."}), 400
+    except Exception as err:
+        print(err)
+        return jsonify({'message': 'Error en la consulta a la base de datos'}), 500
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
