70b282a7eb9a9c7763e86296f9d67988cc18b731
joztos <66192875+joztos@users.noreply.github.com>
2023-07-30 13:55:47 -0600

    salvadosdenuevo

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import json
+import os
 import requests
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import openai
 
 app = Flask(__name__)
 CORS(app)
+
+openai.api_key = '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'
 
 # Load the mapping at startup
 with open('mapeo.json', 'r') as f:
@@ -25,7 +29,7 @@ def getDestrezas():
         asignaturasub = filters["asignaturasub"]
 
         response = requests.get(
-            f'{database_url}/DestrezasOne?select="DESTREZAS%20CON%20CRITERIO%20DE%20DESEMPE%C3%91O"&asignaturasub=eq.{asignaturasub}', 
+            f'{database_url}/DestrezasOne?select=id,"DESTREZAS CON CRITERIO DE DESEMPEÑO"&asignaturasub=eq.{asignaturasub}', 
             headers=headers
         )
 
@@ -33,15 +37,13 @@ def getDestrezas():
             print(f"Error response from database: {response.content}")
             return jsonify({'message': 'Error en la consulta a la base de datos'}), 500
 
-        # Create a list of objects, each with an 'id' and a 'nombre'
-        destrezas = [{'id': row['id'], 'nombre': row['DESTREZAS CON CRITERIO DE DESEMPEÑO']} for row in response.json()]
+        destrezas = [{"id": row['id'], "name": row['DESTREZAS CON CRITERIO DE DESEMPEÑO']} for row in response.json()]
         return jsonify({'destrezas': destrezas}), 200
     except KeyError:
         return jsonify({'message': "Error: Falta uno o más detalles requeridos en los filtros."}), 400
     except Exception as err:
         print(err)
         return jsonify({'message': 'Error en la consulta a la base de datos'}), 500
-
 
 @app.route('/api/getIndicadores', methods=["POST"])
 def getIndicadores():
@@ -50,10 +52,10 @@ def getIndicadores():
         return jsonify({'message': 'Bad Request: JSON data required'}), 400
 
     try:
-        destreza_id = filters["destreza_id"]  # change "criterio_de_evaluacioni" to "destreza_id"
+        destreza_id = filters["destreza_id"]
 
         # Look up the indicators in the mapping
-        indicadores = mapeo.get(str(destreza_id), [])  # ensure the id is in string format
+        indicadores = mapeo.get(str(destreza_id), [])
 
         return jsonify({'indicadores': indicadores}), 200
     except KeyError:
@@ -61,6 +63,32 @@ def getIndicadores():
     except Exception as err:
         print(err)
         return jsonify({'message': 'Error en la consulta a la base de datos'}), 500
+
+@app.route('/api/generateMicroPlan', methods=["POST"])
+def generateMicroPlan():
+    data = request.get_json()
+    destreza = data.get("destreza")
+    indicador = data.get("indicador")
+
+    if not destreza or not indicador:
+        return jsonify({'message': 'Error: destreza and indicador are required.'}), 400
+
+    message = f"Create a class microplan based on the following skills: {destreza}, and indicators: {indicador}."
+
+    try:
+        response = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": message}
+            ]
+        )
+        generatedPlan = response['choices'][0]['message']['content'].strip()
+        return jsonify({'microplan': generatedPlan}), 200
+
+    except Exception as e:
+        print(e)
+        return jsonify({'message': 'Error generating microplan.'}), 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
