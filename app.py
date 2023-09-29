81630c400b8300f7acbf10ca1ff057cb4949af13
joztos <66192875+joztos@users.noreply.github.com>
2023-09-28 18:41:04 -0600

    aaa

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import json
-import requests
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
-import openai
+from guidance.llms import OpenAI
+from llama_index.program import GuidancePydanticProgram
+from pydantic import BaseModel
+from typing import List
 
+# Configuración
 app = Flask(__name__)
 CORS(app)
 
@@ -17,65 +20,78 @@ headers = {
     "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtdGZzdWNucmZpdXZjeXdrZHFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY4NTAyNzYsImV4cCI6MjAwMjQyNjI3Nn0.sQxAXBaNmYtfAPPKggSwy79LNKv_gEbVodzWTm7RzhY"
 }
 
-@app.route('/api/getDestrezas', methods=["POST"])
-def getDestrezas():
-    filters = request.get_json()
-    asignaturasub = filters.get("asignaturasub")
+class EntradaPlaneacion(BaseModel):
+    destreza: str
+    indicador: str
+    metodologia: str
+    temporalidad: int  # Representa el número de lecciones pedagógicas a generar
 
-    response = requests.get(
-        f'{database_url}/DestrezasOne?select=id,"DESTREZAS CON CRITERIO DE DESEMPEÑO"&asignaturasub=eq.{asignaturasub}', 
-        headers=headers
-    )
+class Actividad(BaseModel):
+    titulo: str
+    descripcion: str
+    duracion: int
 
-    if response.status_code != 200:
-        return jsonify({'message': 'Error en la consulta a la base de datos'}), 500
+class Bloque(BaseModel):
+    nombre: str
+    actividades: List[Actividad]  # Cada bloque (lección pedagógica) contiene varias actividades
 
-    destrezas = [{"id": row['id'], "name": row['DESTREZAS CON CRITERIO DE DESEMPEÑO']} for row in response.json()]
-    return jsonify({'destrezas': destrezas}), 200
-
-@app.route('/api/getIndicadores', methods=["POST"])
-def getIndicadores():
-    filters = request.get_json()
-    destreza_id = filters.get("destreza_id")
-    indicadores = mapeo.get(str(destreza_id), [])
-    return jsonify({'indicadores': indicadores}), 200
-
-@app.route('/api/generateMicroPlan', methods=["POST"])
-def generateMicroPlan():
-    data = request.get_json()
-    destreza = data.get("destreza")
-    indicador = data.get("indicador")
-    message = f"Create a class microplan based on the skill: {destreza}, and indicator: {indicador}."
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": message}
-        ]
-    )
-    generatedPlan = response['choices'][0]['message']['content'].strip()
-    return jsonify({'microplan': generatedPlan}), 200
-
+class SalidaPlaneacion(BaseModel):
+    objetivo: str
+    bloques: List[Bloque]  # La cantidad de bloques es controlada por el valor de 'temporalidad'
 @app.route('/api/generateFullPlan', methods=["POST"])
-def generateFullPlan():
-    data = request.get_json()
-    destreza = data.get("destreza")
-    indicador = data.get("indicador")
-    temporalidad = data.get("temporalidad")
-    metodologia = data.get("metodologia")
+def generate_full_plan():
+    try:
+        data = request.get_json()
+        entrada = EntradaPlaneacion(**data)
+        
+        # Formatear el prompt con valores específicos del usuario
+        prompt_template_str = f"""{{
+    "objetivo": "Generar un plan de clase con destreza: {{entrada.destreza}}, indicador: {{entrada.indicador}}, metodología: {{entrada.metodologia}} y temporalidad: {{entrada.temporalidad}}",
+    "plan": {{
+        "titulo": "# 📚 Plan de Clase:",
+        "objetivo_de_clase": {{
+            "encabezado": "## 🚀 Objetivo de Clase:",
+            "descripcion": "Proporcionar el objetivo a conseguir en relación con la destreza: '{{entrada.destreza}}' y el indicador: '{{entrada.indicador}}'."
+        }},
+        ...
+}}
+"""
 
-    message = f"Create a {temporalidad} class plan using the {metodologia} methodology based on the skill: {destreza}, and indicator: {indicador}."
+                
+                "actividades": {{
+                    "encabezado": "## ✅ Actividades:",
+                    "detalle": [{{#geneach 'actividades' num_iterations={{{entrada.temporalidad}}}}}
+                        {{"minuto_a_minuto": "{{gen 'actividad'}}"}}
+                    {{/geneach}}]
+                }},
+                "evaluacion": {{
+                    "encabezado": "## ✅ Evaluación:",
+                    "preguntas": [{{#geneach 'evaluacion' num_iterations=5}}
+                        {{"pregunta": "{{gen 'pregunta'}}", "opciones": [{{#geneach 'opciones' num_iterations=4}}
+                            "{{gen 'opcion'}}"{{/geneach}}]
+                        ]}}
+                    {{/geneach}}]
+                }},
+                "dinamica": {{
+                    "encabezado": "## 🎨 Dinámica:",
+                    "propuesta": "{{gen 'dinamica'}}",
+                    "materiales": [{{#geneach 'materiales' num_iterations=3}}
+                        "{{gen 'material'}}"{{/geneach}}
+                    ]}
+                }}
+            }}
+        }}"""
+        
+        program = GuidancePydanticProgram(
+            output_cls=SalidaPlaneacion,
+            prompt_template_str=prompt_template_str,
+            guidance_llm=OpenAI("GPT-3.5"),
+            verbose=True,
+        )
+        
+        salida = program()  # Generar salida basada en el modelo Pydantic
+        return jsonify(salida.dict()), 200
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": message}
-        ]
-    )
-    generatedPlan = response['choices'][0]['message']['content'].strip()
-    return jsonify({'plan': generatedPlan}), 200
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    except Exception as e:
+        print(e)
+        return jsonify({'message': 'Error generating plan.'}), 500
