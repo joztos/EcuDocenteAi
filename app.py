59c0509ec7f37849bdb318e4eed27e070e498786
joztos <66192875+joztos@users.noreply.github.com>
2023-10-01 22:57:11 -0600

    revisa

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify, make_response
-from flask_cors import CORS  # Ya no necesitas importar cross_origin aquí
+from flask_cors import CORS, cross_origin  # Importa cross_origin
 import traceback
 from supabase_py import create_client
 import guidance
 import json
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # Configura CORS para permitir solicitudes desde localhost:3000
+
 
 
 # Configura y verifica el resto de las variables de entorno y servicios
@@ -15,8 +16,9 @@ SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJ
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)  # Inicializa el cliente Supabase
 
-# Configura el modelo de lenguaje para ejecutar programas de guidance
-guidance.llm = guidance.llms.OpenAI(model="gpt-3.5-turbo", api_key="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")
+# Configura el modelo de lenguaje para ejecutar programas de guidance, # set the default language model that execute guidance programs
+
+guidance.llm = guidance.llms.OpenAI("text-davinci-003", api_key="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")
 
 # Carga el mapeo al inicio
 with open('mapeo.json', 'r') as f:
@@ -61,14 +63,18 @@ def getIndicadores():
 
 
 
+
 @app.route('/api/generateMicroPlan', methods=["POST", "OPTIONS"])
+@cross_origin()  # Usa el decorador @cross_origin() para permitir el acceso
+
 def generateMicroPlan():
-    if request.method == "OPTIONS":
-        response = make_response()
-        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
-        response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        return response
+    
+    if request.method == "OPTIONS":  # Si el método de la solicitud es OPTIONS
+        response = make_response()  # Crea una respuesta vacía
+        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'  # Permite solicitudes desde localhost:3000
+        response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'  # Permite métodos POST y OPTIONS
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'  # Permite encabezados de Content-Type
+        return response  # Retorna la respuesta configurada
     
     try:
         if request.content_type != 'application/json':
@@ -91,6 +97,7 @@ def generateMicroPlan():
 
         guidance_result = program(destreza=destreza, indicador=indicador)
         return jsonify({'generated_plan': guidance_result}), 200
+            # Retorna el resultado
 
     except Exception as e:
         app.logger.error(traceback.format_exc())
