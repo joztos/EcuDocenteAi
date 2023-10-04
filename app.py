62eb41bed8a9ff06ad4938b4cbf8c24105cede22
joztos <66192875+joztos@users.noreply.github.com>
2023-10-04 13:18:58 -0600

    solo un api point

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS, cross_origin
 from supabase_py import create_client
 import guidance
 import json
+import traceback  # Importar la biblioteca al comienzo del archivo
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
@@ -22,42 +23,6 @@ guidance.llm = guidance.llms.OpenAI(model="text-davinci-003", api_key="sk-Q5nTaY
 with open('mapeo.json', 'r') as f:
     mapeo = json.load(f)
 
-@app.route('/api/getskills', methods=['POST'])
-def get_destrezas():
-    try:
-        data = request.json
-        asignatura = data.get('asignatura')
-        
-        if not asignatura:
-            return jsonify({'error': 'Asignatura no proporcionada'}), 400
-        
-        response = supabase.table('oficialecu').select('skill').eq('asignatura', asignatura).execute()
-        
-        if 'error' in response:
-            return jsonify({'error': 'Error al obtener datos de Supabase'}), 500
-        
-        skills = [row['skill'] for row in response.get('data', [])]
-        
-        return jsonify({'destrezas': skills}), 200
-        
-    except Exception as e:
-        return jsonify({'error': 'Error interno del servidor'}), 500
-
-@app.route('/api/getIndicadores', methods=["POST"])
-def getIndicadores():
-    filters = request.get_json()
-    if not filters:
-        return jsonify({'message': 'Bad Request: JSON data required'}), 400
-    
-    try:
-        destreza_id = filters["destreza_id"]
-        indicadores = mapeo.get(str(destreza_id), [])
-        return jsonify({'indicadores': indicadores}), 200
-    except KeyError:
-        return jsonify({'message': "Error: Falta uno o más detalles requeridos en los filtros."}), 400
-    except Exception as err:
-        return jsonify({'message': 'Error en la consulta a la base de datos'}), 500
-
 @app.route('/api/generateMicroPlan', methods=["POST", "OPTIONS"])
 @cross_origin()
 def generateMicroPlan():
@@ -67,7 +32,7 @@ def generateMicroPlan():
         response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
         return response
-    
+
     try:
         if request.content_type != 'application/json':
             return jsonify({'error': 'Content-Type must be application/json'}), 415
@@ -76,24 +41,21 @@ def generateMicroPlan():
         destreza = data.get('destreza', '').strip()
         indicador = data.get('indicador', '').strip()
         metodologia = data.get('metodologia', '').strip()
-        temporalidad = data.get('temporalidad', '').strip()
+        temporalidad = int(data.get('temporalidad', 1))  # Default to 1 if not provided
 
-        if not destreza or not indicador or not metodologia or not temporalidad:
-            return jsonify({'error': 'Todos los campos son requeridos y no pueden estar vacíos'}), 400
+        if not destreza or not indicador or not metodologia:
+            return jsonify({'error': 'Destreza, indicador y metodología son requeridos y no pueden estar vacíos'}), 400
 
         plans = []
-        num_blocks = 1 if temporalidad == "dia" else 5
-        for i in range(num_blocks):
-            day_or_block = f"Día {i+1}" if temporalidad == "semana" else ""
-            program = guidance(f'''
-            Plan de Clase {day_or_block} para la destreza: {{destreza}}, el indicador: {{indicador}}, y la metodología: {metodologia}
+        for i in range(temporalidad):
+            program = guidance('''
             🚀 Objetivo de Clase: "{{gen 'objetivo' max_tokens=80}}"
             ✅ Actividades: "{{gen 'actividades' max_tokens=500}}"
             ✅ Evaluación: "{{gen 'evaluacion' max_tokens=300}}"
             🎨 Dinámica: "{{gen 'dinamica' max_tokens=400}}"
             ''')
 
-            guidance_result = program(destreza=destreza, indicador=indicador, metodologia=metodologia, temporalidad=temporalidad)
+            guidance_result = program(destreza=destreza, indicador=indicador, metodologia=metodologia)
             result_dict = {k: v for k, v in guidance_result.variables().items() if k != "llm"}
             plans.append(result_dict)
 
